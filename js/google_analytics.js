fad83c6a983a5915c25ad5f8b321f724162fbed7
svenbergryen <svenryen@gmail.com>
2018-05-21 18:51:26 +0200

    Revert "Issue #2933614 by svenryen, MaskOta, GaëlG, jhuhta, phjou: How to block cookies until the user accepts, for example Google Analytics cookies?"
    This reverts commit ff2fa2ae5ae8199b6d82d05a7b374adff0b4ce89.

--- js/google_analytics.js
+++
@@ -1,5 +0,0 @@
-(function ($, Drupal, drupalSettings) {
-  if (drupalSettings.eu_cookie_compliance.GA_id && !Drupal.eu_cookie_compliance.hasAgreed()) {
-    window['ga-disable-' + drupalSettings.eu_cookie_compliance.GA_id] = true;
-  }
-})(jQuery, Drupal, Drupal.settings);
